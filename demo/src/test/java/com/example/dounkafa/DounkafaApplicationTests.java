e294645e9e6a6d05bfdcd723a3b81fc077d3c681
Fatimata Diagouraga <gousstatissta@gmail.com>
2022-09-13 14:13:26 +0000

    premier commit

---
+++ demo/src/test/java/com/example/dounkafa/DounkafaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dounkafa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DounkafaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
